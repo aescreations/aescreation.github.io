c5233ebd157d9d04e8403f31aecc39c36a337ba0
aescreations <50584042+aescreations@users.noreply.github.com>
2019-06-26 12:42:20 -0300

    New site

--- js/main.js
+++ js/main.js
@@ -1,172 +1,70 @@
-/* =================================
-------------------------------------
-	ProDent - Dentist Template
-	Version: 1.0
- ------------------------------------
- ====================================*/
+currentStaff = 0;
+
+var staff = [];
+var s1 = {
+    name: "Dean Zimberg",
+    image: "images/team/dean.jpeg",
+    title: "CO-FOUNDER & EXECUTIVE PRESIDENT",
+    linkedin: "-",
+    twitter: "-",
+    email: "-"
+}
+
+var s2 = {
+    name: "Becca Roskill",
+    image: "images/team/becca.jpg",
+    title: "CO-FOUNDER & CEO",
+    linkedin: "",
+    twitter: "-",
+    email: "-"
+}
+
+var s3 = {
+    name: "Robert Epstein",
+    image: "images/team/robbie.jpg",
+    title: "Co-Founder & Executive Director/Chairman",
+    linkedin: "-",
+    twitter: "-",
+    email: "-"
+}
+
+var s4 = {
+    name: "Aram Baghdassarian",
+    image: "images/team/aram.jpg",
+    title: "Co-Founder & Head of Development",
+    linkedin: "-",
+    twitter: "-",
+    email: "-"
+}
+
+staff.push(s1);
+staff.push(s2);
+staff.push(s3);
+staff.push(s4);
 
 
-'use strict';
+function leftClick() {
+    currentStaff--;
+    if(currentStaff < 0) {
+        currentStaff = staff.length - 1;
+    }
+
+    //alert(staff[currentStaff].name);
 
 
-$(window).on('load', function() {
-	/*------------------
-		Preloder
-	--------------------*/
-	$(".loader").fadeOut();
-	$("#preloder").delay(400).fadeOut("slow");
+    document.getElementById("staffName").innerHTML = staff[currentStaff].name;
+    document.getElementById("staffImage").setAttribute("src", staff[currentStaff].image);
+    document.getElementById("staffTitle").innerHTML = staff[currentStaff].title;
+}
 
-});
+function rightClick() {
+    currentStaff++;
+    if(currentStaff >= staff.length) {
+        currentStaff = 0;
+    }
 
-(function($) {
-	/*------------------
-		Navigation
-	--------------------*/
-	$('.nav-switch').on('click', function(event) {
-		$(this).toggleClass('active');
-		$('.nav-warp').slideToggle(400);
-		event.preventDefault();
-	});
-
-
-	/*------------------
-		Background Set
-	--------------------*/
-	$('.set-bg').each(function() {
-		var bg = $(this).data('setbg');
-		$(this).css('background-image', 'url(' + bg + ')');
-	});
-
-
-	/*------------------
-		Progress Bar
-	--------------------*/
-	$('.progress-bar-style').each(function() {
-		var progress = $(this).data("progress");
-		var bgcolor = $(this).data("bgcolor");
-		var prog_width = progress + '%';
-		if (progress <= 100) {
-			$(this).append('<div class="bar-inner" style="width:' + prog_width + '; background: '+ bgcolor +';"><span>' + prog_width + '</span></div>');
-		}
-		else {
-			$(this).append('<div class="bar-inner" style="width:100%; background: '+ bgcolor +';"><span>100%</span></div>');
-		}
-	});
-
-
-
-
-	/*------------------
-		Testimonials
-	--------------------*/
-	$('.testimonials-slider').owlCarousel({
-		loop: true,
-		nav: false,
-		dots: true,
-		margin: 128,
-		center:true,
-		items: 1,
-		mouseDrag: false,
-		animateOut: 'fadeOutRight',
-		animateIn: 'fadeInLeft',
-		autoplay:true
-	});
-
-
-	/*------------------
-		Brands Slider
-	--------------------*/
-	$('.brands-slider').owlCarousel({
-		loop: true,
-		nav: false,
-		dots: false,
-		margin : 40,
-		autoplay: true,
-		responsive : {
-			0 : {
-				items: 1,
-			},
-			480 : {
-				items: 2,
-			},
-			768 : {
-				items: 4,
-			},
-			1200 : {
-				items: 5,
-			}
-		}
-	});
-
-
-
-	/*------------------
-		Popular Services
-	--------------------*/
-	$('.popular-services-slider').owlCarousel({
-		loop: true,
-		dots: false,
-		margin : 40,
-		autoplay: true,
-		nav:true,
-		navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
-		responsive : {
-			0 : {
-				items: 1,
-			},
-			768 : {
-				items: 2,
-			},
-			991: {
-				items: 3
-			}
-		}
-	});
-
-
-	/*------------------
-		Accordions
-	--------------------*/
-	$('.panel-link').on('click', function (e) {
-		$('.panel-link').removeClass('active');
-		var $this = $(this);
-		if (!$this.hasClass('active')) {
-			$this.addClass('active');
-		}
-		e.preventDefault();
-	});
-
-
-	/*------------------
-		Circle progress
-	--------------------*/
-	$('.circle-progress').each(function() {
-		var cpvalue = $(this).data("cpvalue");
-		var cpcolor = $(this).data("cpcolor");
-		var cptitle = $(this).data("cptitle");
-		var cpid 	= $(this).data("cpid");
-
-		$(this).append('<div class="'+ cpid +' loader-circle"></div><div class="progress-info"><h2>'+ cpvalue +'%</h2><p>'+ cptitle +'</p></div>');
-
-		if (cpvalue < 100) {
-
-			$('.' + cpid).circleProgress({
-				value: '0.' + cpvalue,
-				size: 110,
-				thickness: 7,
-				fill: cpcolor,
-				emptyFill: "rgba(0, 0, 0, 0)"
-			});
-		} else {
-			$('.' + cpid).circleProgress({
-				value: 1,
-				size: 110,
-				thickness: 7,
-				fill: cpcolor,
-				emptyFill: "rgba(0, 0, 0, 0)"
-			});
-		}
-
-	});
-
-})(jQuery);
+    //alert(staff[currentStaff].name);
+    document.getElementById("staffName").innerHTML = staff[currentStaff].name;
+    document.getElementById("staffTitle").innerHTML = staff[currentStaff].title;
+    document.getElementById("staffImage").setAttribute("src", staff[currentStaff].image);
+}
